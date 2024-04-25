256ae23b0501f1b4801740784195ad01554f16cf
mubin <mubin.ice.ru@gmail.com>
2024-04-26 03:37:25 +0600

    add mail service module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MailModule } from './mail/mail.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/microfinanace'),
-    UserModule
+    UserModule,
+    MailModule
   ]
 })
 export class AppModule {}
