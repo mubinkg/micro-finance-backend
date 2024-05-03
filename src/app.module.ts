60fc7a8a5dafd09b455535fdb4b232412cfbba4f
mubin <mubin.ice.ru@gmail.com>
2024-05-03 21:23:54 +0600

    add upload module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MailModule } from './mail/mail.module';
 import { LoanModule } from './loan/loan.module';
+import { UploadModule } from './upload/upload.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/microfinanace'),
     UserModule,
     MailModule,
-    LoanModule
+    LoanModule,
+    UploadModule
   ]
 })
 export class AppModule {}
