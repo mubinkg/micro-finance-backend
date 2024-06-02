617c2eb578ee675d5abdcf02444d98411c28839b
mubin <mubin.ice.ru@gmail.com>
2024-06-02 18:38:34 +0600

    add payments schema

--- src/payments/entities/payment.entity.ts
+++ src/payments/entities/payment.entity.ts
@@ -1,1 +1,26 @@
-export class Payment {}
+import { Prop, Schema } from "@nestjs/mongoose";
+import mongoose from "mongoose";
+import { Loan } from "src/loan/entities/loan.entity";
+import { User } from "src/user/entities/user.entity";
+
+@Schema({
+    timestamps: true
+})
+export class Payment {
+    _id: string
+
+    @Prop({type:mongoose.Schema.Types.ObjectId, ref:'User'})
+    user: User
+
+    @Prop({type: mongoose.Schema.Types.ObjectId, ref:'Loan'})
+    loan: Loan
+
+    @Prop({type: Number})
+    paidAmount: number
+
+    @Prop({type: Number})
+    unpaidAmount: number
+
+    createdAt: Date
+    updatedAt: Date
+}
