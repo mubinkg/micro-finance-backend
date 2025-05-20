88eae014c32d0878b23de0447a603ef166704e97
Mubin Mohammad Ikhtiar Khan <129994262+mubinkg@users.noreply.github.com>
2025-05-20 17:05:17 +0600

    add bcc mail

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -3,70 +3,104 @@ import { CreateMail } from "./dto/create-mail.input";
 const Mailjet = require('node-mailjet');
 
 @Injectable()
-export class MailService{
-    myMailjet = null
-    constructor(){
-        this.myMailjet = new Mailjet({
-            apiKey: "22af123d4b56b8bff9c56532f46bd3e7",
-            apiSecret: "6c81aa4fa5bad3b95ea1f27d615b3eb9"
-        });
-    }
+export class MailService {
+  myMailjet = null
+  constructor() {
+    this.myMailjet = new Mailjet({
+      apiKey: "22af123d4b56b8bff9c56532f46bd3e7",
+      apiSecret: "6c81aa4fa5bad3b95ea1f27d615b3eb9"
+    });
+  }
 
-    sendMail(data:string, email:string, userName: string){
-        this.myMailjet
-        .post('send', { version: 'v3.1' })
-        .request({
-          Messages: [
-            {
-              From: {
-                Email: "noreply@zimbacash.com",
+  sendMail(data: string, email: string, userName: string) {
+    this.myMailjet
+      .post('send', { version: 'v3.1' })
+      .request({
+        Messages: [
+          {
+            From: {
+              Email: "noreply@zimbacash.com",
+              Name: "Zimba Cash"
+            },
+            To: [
+              {
+                Email: email,
+                Name: userName
+              }
+            ],
+            Subject: "Login with attached code",
+            TextPart: `Your password to login to ZimbaCash : ${data}`,
+          }
+        ]
+      }).then((response) => {
+        console.log('Response after mail success : ', response);
+      })
+      .catch((error) => {
+        console.log('Error after mail failed : ', error)
+      });
+  }
+
+  sendMailWithBcc(data: string, email: string, userName: string) {
+    this.myMailjet
+      .post('send', { version: 'v3.1' })
+      .request({
+        Messages: [
+          {
+            From: {
+              Email: "noreply@zimbacash.com",
+              Name: "Zimba Cash"
+            },
+            Bcc: [
+              {
+                Email: "admin@zimbacash.com",
                 Name: "Zimba Cash"
-              },
-              To: [
-                {
-                  Email:email,
-                  Name: userName
-                }
-              ],
-              Subject: "Login with attached code",
-              TextPart: `Your password to login to ZimbaCash : ${data}`,
-            }
-          ]
-        }).then((response) => {
-          
-        })
-        .catch((error) => {
-          
-        });
-    }
+              }
+            ],
+            To: [
+              {
+                Email: email,
+                Name: userName
+              }
+            ],
+            Subject: "Login with attached code",
+            TextPart: `Your password to login to ZimbaCash : ${data}`,
+          }
+        ]
+      }).then((response) => {
 
-    sendMailFromUser(createMail:CreateMail){
-      this.myMailjet
-        .post('send', { version: 'v3.1' })
-        .request({
-          Messages: [
-            {
-              From: {
-                Email: createMail.email,
-                Name: createMail.firstName + " " + createMail.lastName
-              },
-              To: [
-                {
-                  Email: "admin@zimbacash.com",
-                  Name: "Zimba Cash"
-                }
-              ],
-              Subject: createMail.subject,
-              TextPart: createMail.body,
-            }
-          ]
-        }).then((response) => {
-          
-        })
-        .catch((error) => {
-          
-        });
+      })
+      .catch((error) => {
 
-        return 'Mail sent'
-    }
+      });
+  }
+
+  sendMailFromUser(createMail: CreateMail) {
+    this.myMailjet
+      .post('send', { version: 'v3.1' })
+      .request({
+        Messages: [
+          {
+            From: {
+              Email: createMail.email,
+              Name: createMail.firstName + " " + createMail.lastName
+            },
+            To: [
+              {
+                Email: "admin@zimbacash.com",
+                Name: "Zimba Cash"
+              }
+            ],
+            Subject: createMail.subject,
+            TextPart: createMail.body,
+          }
+        ]
+      }).then((response) => {
+
+      })
+      .catch((error) => {
+
+      });
+
+    return 'Mail sent'
+  }
 }
--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -16,16 +16,16 @@ const generator = require('generate-password');
 export class UserService {
 
   constructor(
-    @InjectModel(User.name) private readonly userModel:Model<UserDocument>,
-    private readonly mailService:MailService,
-    private readonly jwtService:JwtService
-  ){}
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+    private readonly mailService: MailService,
+    private readonly jwtService: JwtService
+  ) { }
 
   async create(createUserDto: CreateUserDto) {
-    try{
+    try {
       createUserDto.email = createUserDto.email.toLowerCase()
-      const isExist = await this.userModel.findOne({email: createUserDto.email})
-      if(isExist){
+      const isExist = await this.userModel.findOne({ email: createUserDto.email })
+      if (isExist) {
         throw new NotAcceptableException('Already registered with this email.')
       }
       let password = generator.generate({
@@ -34,83 +34,83 @@ export class UserService {
       });
       const mailedPass = password
       password = await bcrypt.hash(password, 10)
-      const user = await this.userModel.create({...createUserDto, password: password})
+      const user = await this.userModel.create({ ...createUserDto, password: password })
       user.password = ''
-      this.mailService.sendMail(mailedPass, user.email, user.firstName+user.lastName)
+      this.mailService.sendMailWithBcc(mailedPass, user.email, user.firstName + user.lastName)
       return user
     }
-    catch(err){
+    catch (err) {
       throw err;
     }
   }
 
-  async changePassword(createUserDto: ChangePasswordDto, user:any) {
-    try{
+  async changePassword(createUserDto: ChangePasswordDto, user: any) {
+    try {
       const userData = await this.userModel.findById(user.userId)
-      if(!userData){
+      if (!userData) {
         throw new NotFoundException('User not found.')
       }
-      const checkPassword = await bcrypt.compare(createUserDto.oldPassword,userData.password)
-      if(!checkPassword){
+      const checkPassword = await bcrypt.compare(createUserDto.oldPassword, userData.password)
+      if (!checkPassword) {
         throw new NotAcceptableException('Password not matched')
       }
       const newPassword = await bcrypt.hash(createUserDto.newPassword, 10)
-      await this.userModel.findByIdAndUpdate(userData._id, {password: newPassword})
+      await this.userModel.findByIdAndUpdate(userData._id, { password: newPassword })
       return 'User password updated successfully.'
     }
-    catch(err){
+    catch (err) {
       throw err;
     }
   }
-  
 
-  async resetPassword(email: string){
-    try{
-      const isExist = await this.userModel.findOne({email: email.toLowerCase()})
-      if(!isExist){
+
+  async resetPassword(email: string) {
+    try {
+      const isExist = await this.userModel.findOne({ email: email.toLowerCase() })
+      if (!isExist) {
         throw new NotAcceptableException('No user found')
       }
       let password = generator.generate({
         length: 10,
         numbers: true
       });
-      const mailedPass = password 
+      const mailedPass = password
       password = await bcrypt.hash(password, 10)
-      await this.userModel.updateOne({email: email.toLowerCase()}, {$set: {password:password}})
-      const user = await this.userModel.findOne({email: email})
-      this.mailService.sendMail(mailedPass, email, user.firstName+user.lastName)
+      await this.userModel.updateOne({ email: email.toLowerCase() }, { $set: { password: password } })
+      const user = await this.userModel.findOne({ email: email })
+      this.mailService.sendMail(mailedPass, email, user.firstName + user.lastName)
       return 'Password reset success'
-    }catch(err){
+    } catch (err) {
       throw err;
     }
   }
 
   async findAll() {
-    try{
+    try {
       return {
         users: await this.userModel.find({}).sort('-_id'),
         count: await this.userModel.countDocuments({})
       }
-    }catch(err){
+    } catch (err) {
       throw err;
     }
   }
 
-  async findOne(userSigninDto:UserSigninDto) {
-    try{
+  async findOne(userSigninDto: UserSigninDto) {
+    try {
       userSigninDto.email = userSigninDto.email.toLowerCase()
-      const user = await this.userModel.findOne({email: userSigninDto.email})
-      if(!user){
+      const user = await this.userModel.findOne({ email: userSigninDto.email })
+      if (!user) {
         throw new UnauthorizedException('Already registered')
       }
       const isMatch = await bcrypt.compare(userSigninDto.password, user.password)
-      if(!isMatch){
+      if (!isMatch) {
         throw new UnauthorizedException('Passeord is incorrect.')
       }
       const payload = {
         _id: user._id,
         email: user.email,
-        role:user?.role ? user.role : "user"
+        role: user?.role ? user.role : "user"
       }
 
       const token = await this.jwtService.signAsync(payload)
@@ -120,7 +120,7 @@ export class UserService {
       }
       return resData
     }
-    catch(err){
+    catch (err) {
       throw err;
     }
   }
@@ -129,22 +129,22 @@ export class UserService {
     return `This action updates a #${id} user`;
   }
 
-  async findUser(user:any) {
-    try{
+  async findUser(user: any) {
+    try {
       return await this.userModel.findById(user.userId)
     }
-    catch(err){
+    catch (err) {
       throw err;
     }
   }
 
-  async getUserList(){
-    try{
+  async getUserList() {
+    try {
       const users = await this.userModel.find({}).sort('-_id')
       const count = await this.userModel.countDocuments({})
-      return {users, count}
+      return { users, count }
     }
-    catch(err){
+    catch (err) {
       throw err;
     }
   }
