97cffe560c50962f7adc136baeb020fd80292625
Mubin Mohammad Ikhtiar Khan <129994262+mubinkg@users.noreply.github.com>
2025-07-12 00:30:48 +0600

    update host and port

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ async function bootstrap() {
     origin: '*',
   });
   app.use(morgan(':date[clf] ":method :url"'));
-  await app.listen(3000);
+  const port = process.env.PORT || 3000; // Use environment variable or default to 3000
+  await app.listen(port, '0.0.0.0'); // Explicitly bind to 0.0.0.0
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
